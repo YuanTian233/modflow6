282e55f2ab2560c78b5b65ea1a86137ed4aa1ed1
YuanTian233 <139559072+YuanTian233@users.noreply.github.com>
2023-11-30 14:44:03 +0100

    Add files via upload

--- ver3.1/MC_ref.py
+++ ver3.1/MC_ref.py
@@ -30,6 +30,9 @@ from gwm import hkfields
 from gwm import runmodelflow
 
 
+exe_name = 'C:/Users/tian/Desktop/mf6.4.2/bin/mf6.exe'  #remamber to change this path where you modflow 6 is.
+model_name = "50"  #a name you like, not too long.
+    
  
 n_reali =  1 #this is the MC size
 
@@ -38,13 +41,16 @@ n_zones = 6 # i got a zoned model, so i have 6 parameter
 
 gm_mean = math.log(1e-5)  # ln(K) average --> constant
 gm_var = 1.0  # ln(K) variance --> constant
+
+#parameter genaration (log(HK))
 parameter_sets = np.random.normal(loc=gm_mean, 
                                       scale=gm_var,
                                       size=(n_reali, n_zones)
                                       )
 obs = pd.read_csv((model_output_path + '/input/obs_hk.csv'),header = 0, index_col = 0)
 obs= np.array(obs)
-    
+
+#load the distubution flie    
 zones_vec50x50 = pd.read_csv((model_output_path + '/input/zone_distribution.csv'), dtype=np.float64)
 zones_vec = np.array(zones_vec50x50, dtype = float)
 
@@ -54,8 +60,7 @@ df.to_csv(os.path.join(model_output_path+ r'/output','parameter_sets.csv'))
 
 ####if you want the ture results you can use this data as the input in line 77
 mat_file_path = (model_output_path + '/ref_data/Y_true.mat')
-# Load the .mat file
-mat_contents = scipy.io.loadmat(mat_file_path)
+mat_contents = scipy.io.loadmat(mat_file_path) # Load the .mat file
 y_true = mat_contents['Y_true']
 
 count = 0
@@ -64,8 +69,6 @@ concentration  = []
 
 for count in range(n_reali):
     # tmpdir = tempfile.mkdtemp()
-    model_name = "50"  
-    exe_name = 'C:/Users/tian/Desktop/mf6.4.2/bin/mf6.exe'  #remamber to change this path.
     sim_name = f'{model_name}_{count}' #simulation name of each model
     ws = os.path.join('model',sim_name) #worksapce for each model
     gwfname = "f" + sim_name # a name you can change for groundwater flow model. Name maximum length of 16
